a01b9a8d41fd350c6d258751a2e6c11efb1c5a16
Tomiko <554685+tetrachrome@users.noreply.github.com>
2020-07-01 22:03:18 -0700

    Make background image of homepage dynamic

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@
  * Updated  : Jul 01, 2020
  */
 
-import React from 'react';
+import React, { Component } from 'react';
 
 import "semantic-ui-css/semantic.min.css";
 
@@ -15,36 +15,49 @@ import { Button, Container, Header, Menu, Segment } from 'semantic-ui-react'
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Segment inverted vertical textAlign="center" className="mainSegment">
-        <Container as="nav">
-          <Header inverted as="h1">
-            Chewbaaka
-          </Header>
-          <Menu borderless compact inverted>
-            {/* <Menu.Item active>Home</Menu.Item> */}
-            <Menu.Item>IUCN status</Menu.Item>
-            <Menu.Item>CCF</Menu.Item>
-          </Menu>
-        </Container>
-        <Container className="content">
-          <Header inverted as="h1">
-            Run cheetah run
-          </Header>
-          <p>
-            Visit and explore CCF's <b>Cheetah Museum</b> at the
-            comfort of your home.
-          </p>
-          <Button size="huge">Explore</Button>
-        </Container>
-        {/* <Segment inverted vertical as="footer">
-          by @Tomiko.
-        </Segment> */}
-      </Segment>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Segment inverted vertical textAlign="center" className={this.centerSegmentStyleClassName()}>
+          <Container as="nav">
+            <Header inverted as="h1">
+              Chewbaaka
+            </Header>
+            <Menu borderless compact inverted>
+              {/* <Menu.Item active>Home</Menu.Item> */}
+              <Menu.Item>IUCN status</Menu.Item>
+              <Menu.Item>CCF</Menu.Item>
+            </Menu>
+          </Container>
+          <Container className="content">
+            <Header inverted as="h1">
+              Run cheetah run
+            </Header>
+            <p>
+              Visit and explore CCF's <b>Cheetah Museum</b> at the
+              comfort of your home.
+            </p>
+            <Button size="huge">Explore</Button>
+          </Container>
+          {/* <Segment inverted vertical as="footer">
+            by @Tomiko.
+          </Segment> */}
+        </Segment>
+      </div>
+    )
+  }
+
+  centerSegmentStyleClassName() {
+    const centerSegmentStyleClassNames = [
+      "centerSegmentStyle1",
+      "centerSegmentStyle2"
+    ]
+
+    const idx = Math.floor(Math.random() * centerSegmentStyleClassNames.length);
+
+    return centerSegmentStyleClassNames[idx];
+  }
 }
 
 export default App;
