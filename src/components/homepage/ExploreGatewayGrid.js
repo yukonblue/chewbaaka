823de79bb644943d11bab0340b929ff4e4a36f3b
Tomiko <554685+tetrachrome@users.noreply.github.com>
2020-07-04 16:05:21 -0700

    Use custom image assets for explore grid item backgrounds

--- src/components/homepage/ExploreGatewayGrid.js
+++ src/components/homepage/ExploreGatewayGrid.js
@@ -15,14 +15,19 @@ import { Grid } from 'semantic-ui-react'
 
 import ExploreGatewayGridItem from './ExploreGatewayGridItem'
 
+import explore_gateway_grid_item_img_01 from './assets/explore_gateway_grid_item_img_01.jpg'
+import explore_gateway_grid_item_img_02 from './assets/explore_gateway_grid_item_img_02.jpg'
+import explore_gateway_grid_item_img_03 from './assets/explore_gateway_grid_item_img_03.jpg'
+import explore_gateway_grid_item_img_04 from './assets/explore_gateway_grid_item_img_04.jpg'
+
 function ExploreGatewayGrid(props) {
   return (
     // TODO: Update `dstUrl` attribute for grid items.
     <Grid container stackable textAlign="center" columns={4} data-testid="ExploreGatewayGridTestId">
-      <ExploreGatewayGridItem imageUrl="https://cheetah.org/wp-content/uploads/2019/05/iwt_ancientindiancheetahhunt_1000-600-scaled.jpg" title="History" dstUrl="#" />
-      <ExploreGatewayGridItem imageUrl="https://cheetah.org/wp-content/uploads/2019/05/Learn_AboutCheetahs_9_1000-600.jpg" title="Biology" dstUrl="#" />
-      <ExploreGatewayGridItem imageUrl="https://cheetah.org/wp-content/uploads/2019/03/cheetah-conservation-fund-conservancies.jpg" title="Ecology" dstUrl="#" />
-      <ExploreGatewayGridItem imageUrl="https://cheetah.org/wp-content/uploads/2019/05/Learn_AboutCheetahs_11_1000-600.jpg" title="Future" dstUrl="#" />
+      <ExploreGatewayGridItem imageUrl={explore_gateway_grid_item_img_01} title="History" dstUrl="#" />
+      <ExploreGatewayGridItem imageUrl={explore_gateway_grid_item_img_02} title="Biology" dstUrl="#" />
+      <ExploreGatewayGridItem imageUrl={explore_gateway_grid_item_img_03} title="Ecology" dstUrl="#" />
+      <ExploreGatewayGridItem imageUrl={explore_gateway_grid_item_img_04} title="Future"  dstUrl="#" />
     </Grid>
   )
 }
