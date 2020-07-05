1eb77f3fddaa6388170daff431d8fba8dc206f6b
Tomiko <554685+tetrachrome@users.noreply.github.com>
2020-07-05 10:29:28 -0700

    Setup page routing and redirection in 'App.js` and 'TestPage.js'

--- src/components/shared/TestPage.js
+++ src/components/shared/TestPage.js
@@ -4,10 +4,12 @@
  *
  * Author   : Tomiko
  * Created  : Jul 04, 2020
- * Updated  : Jul 04, 2020
+ * Updated  : Jul 05, 2020
  */
 
 import React from 'react';
+
+import { Redirect } from 'react-router-dom'
 
 import "semantic-ui-css/semantic.min.css";
 
@@ -16,8 +18,17 @@ import ImageSlide from './ImageSlide'
 import image01 from './assets/cheetah-conservation-fund-logo.jpg'
 import image02 from './assets/cheetah-conservation-fund-30-logo.svg'
 
+const __DEV__ = ( process.env.NODE_ENV === "development" );
+
 class TestPage extends React.Component {
   render() {
+    /**
+     * Redirect to homepage in production build,
+     * otherwise renders whatever component we need to test here.
+     */
+    if ( !__DEV__ ) {
+      return <Redirect to="/" />  
+    }
     return (
       <ImageSlide images={[image01, image01, image02]} labels={["150 mya", "100 mya", "50 mya"]} percentages={[0, 20,100]} title="Cheetah evolution" decrementButtonLabel="backward" incrementButtonLabel="forward"/>
     )
