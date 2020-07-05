98a59ea07f5dd0044e0185a75c172e38ed9fb6e4
Tomiko <554685+tetrachrome@users.noreply.github.com>
2020-07-05 10:52:49 -0700

    Setup app navigation (#7)
    * Install and setup react-router-dom as dependency in project.
    * Setup page routing and redirection in 'App.js` and 'TestPage.js'.

--- src/App.js
+++ src/App.js
@@ -4,19 +4,34 @@
  *
  * Author   : Tomiko
  * Created  : Jun 30, 2020
- * Updated  : Jul 03, 2020
+ * Updated  : Jul 05, 2020
  */
 
 import React from 'react';
+
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 
 import "semantic-ui-css/semantic.min.css";
 
 import HomePage from './components/homepage/HomePage'
 
+import TestPage from './components/shared/TestPage'
+
 class App extends React.Component {
   render() {
     return (
-      <HomePage/>
+      <Router>
+        <Switch>
+          { /** Route for testing components in development mode. */ }
+          <Route exact path="/test">
+            <TestPage />
+          </Route>
+          <Route exact path="/" >
+            <HomePage />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </Router>
     )
   }
 }
