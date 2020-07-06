61dc44f1f9b4ce127c826a68d91174b48f5da110
Tomiko <554685+tetrachrome@users.noreply.github.com>
2020-07-06 12:06:30 -0700

    Remove unnecessary import

--- src/components/shared/ContentPageHead.js
+++ src/components/shared/ContentPageHead.js
@@ -11,8 +11,6 @@ import React from 'react';
 
 import "semantic-ui-css/semantic.min.css";
 
-import { Menu } from 'semantic-ui-react'
-
 import './ContentPageHead.css'
 import './ContentPageSharedStyles.css'
 
