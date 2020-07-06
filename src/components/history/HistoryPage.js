98f8045be3f4095f616742e120a4b3396f6a64cf
Tomiko <554685+tetrachrome@users.noreply.github.com>
2020-07-06 09:38:59 -0700

    Initial setup of content page skeleton ('ContentPageSkeleton.js')

---
+++ src/components/history/HistoryPage.js
@@ -0,0 +1,49 @@
+/**
+ * HistoryPage.js
+ * Chewbaaka
+ *
+ * Author   : Tomiko
+ * Created  : Jul 06, 2020
+ * Updated  : Jul 06, 2020
+ */
+
+import React from 'react';
+
+import "semantic-ui-css/semantic.min.css";
+
+import ContentPageSkeleton from '../shared/ContentPageSkeleton'
+
+class HistoryPage extends React.Component {
+
+  render() {
+    return (
+      <ContentPageSkeleton content={this.renderContent()}/>
+    )
+  }
+
+  renderContent() {
+    return (
+      <div>
+        <h1>History of the Cheetah</h1>
+        <p>Hello</p>
+              <p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p>
+              <p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p>
+              <p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p>
+              <p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p>
+              <p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p>
+              <p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p>
+              <p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p>
+              <p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p>
+              <p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p>
+              <p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p>
+              <p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p>
+              <p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p>
+              <p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p>
+              <p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p>
+              <p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p><p>Hello</p>
+      </div>
+    )
+  }
+}
+
+export default HistoryPage;
---
+++ src/components/shared/ContentPageSkeleton.js
@@ -0,0 +1,86 @@
+/**
+ * ContentPageSkeleton.js
+ * Chewbaaka
+ *
+ * Author   : Tomiko
+ * Created  : Jul 06, 2020
+ * Updated  : Jul 06, 2020
+ */
+
+import React from 'react';
+
+import "semantic-ui-css/semantic.min.css";
+
+import {
+  Checkbox,
+  // Grid,
+  // Header,
+  // Image,
+  Rail,
+  Ref,
+  // Segment,
+  Sticky,
+  // Container,
+} from 'semantic-ui-react'
+
+import './ContentPageSkeleton.css'
+
+import TopNavBar from './TopNavBar'
+import Footer from './Footer'
+
+class ContentPageSkeleton extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMenuActive : true,
+      contextRef : React.createContext()
+    };
+    this.handleMenuActiveToggle = this.handleMenuActiveToggle.bind(this);
+  }
+
+  handleMenuActiveToggle() {
+    this.setState((prevState) => ({
+      isMenuActive: prevState.isMenuActive ^ 1,
+      contextRef : prevState.contextRef
+    }));
+  }
+
+  render() {
+    return (
+      <div className="ContentPageSkeletonOuterContainer">
+        <TopNavBar />
+        <div>
+          {this.renderBody()}
+        </div>
+        <Footer />
+      </div>
+    )
+  }
+
+  renderBody() {
+    return (
+      <Ref innerRef={this.state.contextRef}>
+        <div>
+          <div className="ContentPageSkeletonContentContainer">
+            {this.props.content}
+          </div>
+          <Rail internal position='left'>
+            <Sticky  context={this.state.contextRef} offset={100}>
+              <Checkbox
+                checked={this.state.isMenuActive}
+                label='Menu'
+                onChange={this.handleMenuActiveToggle}
+                toggle
+                />
+              <br/>
+              TODO: Menu goes here...
+            </Sticky>
+          </Rail>
+        </div>
+      </Ref>
+    )
+  }
+}
+
+export default ContentPageSkeleton;
