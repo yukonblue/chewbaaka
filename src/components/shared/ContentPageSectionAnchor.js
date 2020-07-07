5cee53a91c741f609a25037b148be821392edebd
Tomiko <554685+tetrachrome@users.noreply.github.com>
2020-07-07 15:35:21 -0700

    Add docstring for 'ContentPageSectionAnchor' component

--- src/components/shared/ContentPageSectionAnchor.js
+++ src/components/shared/ContentPageSectionAnchor.js
@@ -7,6 +7,24 @@
  * Updated  : Jul 07, 2020
  */
 
+/**
+ * ContentPageSectionAnchor
+ *
+ * `ContentPageSectionAnchor` is a component that synthesizes
+ * into a local anchor point for the corresponding content page
+ * section within the page.
+ *
+ * Users specify the title of the section, and the title is
+ * used to convert to its corresponding anchor id.
+ * (e.g., "Cheetah and Man" is converted into "cheetah-and-man",
+ * and links can reference to it via "#cheetah-and-man" the href).
+ *
+ * Props:
+ *
+ *  - `title`: The title of the corresponding content section.
+ *    Used to generate the anchor point id.
+ */
+
 import React from 'react';
 
 function ContentPageSectionTitleToAnchorId(title) {
