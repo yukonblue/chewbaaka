763efc465f1ad89ae7f1c5895b58a899f244616d
Tomiko <554685+tetrachrome@users.noreply.github.com>
2020-07-08 23:24:09 -0700

    Enable snapshot testing for History page only locally (#23)

--- src/testing/testing.js
+++ src/testing/testing.js
@@ -15,6 +15,8 @@ const PREDICATE_IS_ON_CIRCLE_CI = () => ( ( CIRCLECI_KEY in process.env )  );
 
 const PREDICATE_IS_ON_CI = () => ( ( CIRCLECI_KEY in process.env || CI_KEY in process.env ) );
 
+const PREDICATE_IS_LOCAL = () => ( ! ( PREDICATE_IS_ON_CI() ) );
+
 const PREDICATE_NO_OP = () => ( true );
 
 const SKIP_TEST_ON_PREDICATE = (testBlock, pred) => {
@@ -32,10 +34,23 @@ const SKIP_TEST_ON_CI = (testBlock) => ( SKIP_TEST_ON_PREDICATE(testBlock, PREDI
 
 const SKIP_TEST = (testBlock) => ( SKIP_TEST_ON_PREDICATE(testBlock, PREDICATE_NO_OP) );
 
+const RUN_TEST_ON_PREDICATE = (pred, callTest) => {
+  if ( pred() ) {
+    callTest();
+  }
+};
+
+const RUN_TEST_ONLY_LOCALLY = (callTest) => {
+  RUN_TEST_ON_PREDICATE(PREDICATE_IS_LOCAL, callTest);
+};
+
 export {
   SKIP_TEST_ON_PREDICATE,
   SKIP_TEST_ON_CIRCLE_CI,
   SKIP_TEST_ON_CI,
+  PREDICATE_IS_LOCAL,
   SKIP_TEST,
-  PREDICATE_NO_OP
+  PREDICATE_NO_OP,
+  RUN_TEST_ON_PREDICATE,
+  RUN_TEST_ONLY_LOCALLY
 };
