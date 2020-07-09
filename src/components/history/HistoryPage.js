3277e8155cb21817976e539460a7bf67cef94215
Tomiko <554685+tetrachrome@users.noreply.github.com>
2020-07-08 19:09:07 -0700

    Move more static content to config file

--- src/components/history/HistoryPage.js
+++ src/components/history/HistoryPage.js
@@ -42,8 +42,8 @@ class HistoryPage extends React.Component {
         <HistoryPageSectionTableOfContent />
 
         <ContentPageIntroParagraph
-          title="History of the Cheetah"
-          content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+          title={config.contentPageIntro.title}
+          content={config.contentPageIntro.content}
         />
 
         {this.renderEvolutionSectionContent()}
@@ -70,8 +70,8 @@ class HistoryPage extends React.Component {
     return (
       <div>
         <ContentPageSectionHead
-          title="Evolution"
-          content="The oldest fossils place  cheetahs in North America in what is now Texas, Nevada and Wyoming. Cheetahs were common throughout Asia, Africa, Europe and North America until the end of the last Ice Age, about 10,000 years ago, when massive climatic changes caused large numbers of mammals to disappear."
+          title={config.contentPageSections["evolution"].intro.title}
+          content={config.contentPageSections["evolution"].intro.content}
         />
 
         <HistoryPageFelidaeFamilyTreeSubsection />
@@ -83,8 +83,8 @@ class HistoryPage extends React.Component {
     return (
       <div>
         <ContentPageSectionHead
-          title="Cheetah and Man"
-          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          title={config.contentPageSections["cheetahAndMan"].intro.title}
+          content={config.contentPageSections["cheetahAndMan"].intro.content}
         />
 
         <HistoryPageCheetahAndManImageSubsection />
@@ -96,8 +96,8 @@ class HistoryPage extends React.Component {
     return (
       <div>
         <ContentPageSectionHead
-          title="Range and Population"
-          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          title={config.contentPageSections["populationAndRange"].intro.title}
+          content={config.contentPageSections["populationAndRange"].intro.content}
         />
 
         <HistoryPageNamibiaSubsection />
--- src/components/history/config.js
+++ src/components/history/config.js
@@ -17,5 +17,29 @@ export const config = {
       "Cheetah and Man",
       "Range and Population"
     ],
+  },
+  contentPageIntro: {
+    title: "History of the Cheetah",
+    content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+  contentPageSections: {
+    evolution: {
+      intro: {
+        title: "Evolution",
+        content: "The oldest fossils place  cheetahs in North America in what is now Texas, Nevada and Wyoming. Cheetahs were common throughout Asia, Africa, Europe and North America until the end of the last Ice Age, about 10,000 years ago, when massive climatic changes caused large numbers of mammals to disappear."
+      }
+    },
+    cheetahAndMan: {
+      intro: {
+        title: "Cheetah and Man",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      }
+    },
+    populationAndRange: {
+      intro: {
+        title: "Range and Population",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      }
+    }
   }
 };
