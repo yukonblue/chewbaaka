1c4eeec9340d3f4bcedb26bd83d323df07171055
Tomiko <554685+tetrachrome@users.noreply.github.com>
2020-07-11 02:25:18 -0700

    Implement and incorporate `ImageCollageDriver` component

--- src/components/shared/ContentPageTail.js
+++ src/components/shared/ContentPageTail.js
@@ -9,7 +9,9 @@
 
 import React from 'react';
 
-import ImageCollage from './ImageCollage'
+import { config } from './config'
+
+import ImageCollageDriver from './ImageCollageDriver'
 
 import {
   getElementStyleClassName,
@@ -22,54 +24,6 @@ import './ContentPageTail.css'
 import './ContentPageSharedStyles.css'
 
 export default class ContentPageTail extends React.Component {
-
-  static images = [
-    "https://th.thgim.com/sci-tech/energy-and-environment/yrktd/article30295376.ece/ALTERNATES/FREE_960/15SMafrican-cubsjpgs",
-    "https://ichef.bbci.co.uk/news/320/cpsprodpb/27A3/production/_96974101_gettyimages-cheetah.jpg",
-    "https://idsb.tmgrup.com.tr/ly/uploads/images/2020/05/20/thumbs/800x531/36753.jpg",
-    "https://travelafricamag.com/wp-content/uploads/2018/01/HR_Mike-Dexter_20171107-IMG_5091.jpg",
-    "https://media.mehrnews.com/d/2019/09/01/4/3224140.jpg",
-    "https://images.theconversation.com/files/152362/original/image-20170111-6447-606129.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=926&fit=clip",
-    "https://www.treehugger.com/thmb/ahEMfgEkro4OBW8hOdenpM3V2bw=/768x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/cheetah-licking-cub-4af516e956004c01a97b4a51175fd8ff.jpg"
-  ];
-
-  static collageCellContent = [
-    {
-      title: "Get Involved",
-      subtitle: "Every action counts",
-      href: "https://cheetah.org/get-involved/ways-to-give/"
-    },
-    {
-      title: "International Cheetah Day",
-      subtitle: "Cheetahs deserve a holiday of their own",
-      href: "https://internationalcheetahday.com/"
-    },
-    {
-      title: "Cheetah on IUCN",
-      subtitle: "Latest status on the species",
-      href: "https://www.iucnredlist.org/es/species/219/13034574"
-    },
-    {
-      title: "Join Forces",
-      subtitle: "Work with other organizations",
-      href: "https://wildnet.org/"
-    },
-    {
-      title: "Take Action",
-      subtitle: "Be a global citizen",
-      href: "https://www.globalcitizen.org/en/take-action/"
-    },
-    {
-      title: "Wildlife Conservation",
-      subtitle: "It's not too late",
-      href: "https://www.worldwildlife.org/initiatives/wildlife-conservation"
-    },
-    {
-      title: "Cheetah on National Geographic",
-      subtitle: "Watch the great documentary",
-      href: "https://www.youtube.com/watch?v=zfyuzX_SAH4"
-    },
-  ];
 
   render() {
     return (
@@ -87,30 +41,11 @@ export default class ContentPageTail extends React.Component {
   }
 
   renderContent() {
-    const count = Math.min(ContentPageTail.images.length, 5);
-
-    let cells = [];
-
-    for (let i = 0; i < count; ++i) {
-      const idx = Math.floor(Math.random() * ContentPageTail.images.length);
-      const content = ContentPageTail.collageCellContent[idx];
-
-      cells.push(
-        {
-          image: ContentPageTail.images[idx],
-          title: content.title,
-          subtitle: content.subtitle,
-          href: content.href,
-          width: 480,
-          height: 320,
-        }
-      );
-    }
-
     return (
       <div className={getElementStyleClassName("ContentPageTailInnerContainer")}>
-        <ImageCollage
-          cells={cells}
+        <ImageCollageDriver
+          data={config.imageCollage}
+          min={5}
         />
       </div>
     );
