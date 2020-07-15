dcaa131995869f2733472b3754c8f66c95221996
Tomiko <554685+tetrachrome@users.noreply.github.com>
2020-07-15 00:30:13 -0700

    Initial setup of Biology page

---
+++ src/components/biology/BiologyPage.js
@@ -0,0 +1,40 @@
+/**
+ * FuturePage.js
+ * Chewbaaka
+ *
+ * Author   : Tomiko
+ * Created  : Jul 15, 2020
+ * Updated  : Jul 15, 2020
+ */
+
+import React from 'react';
+
+import "semantic-ui-css/semantic.min.css";
+
+import ContentPageSkeleton from '../shared/ContentPageSkeleton'
+
+// import FuturePageSectionCheetahAmbassadors from './FuturePage_Section_CheetahAmbassadors'
+
+import { config }  from './config'
+
+export default class FuturePage extends React.Component {
+
+  render() {
+    let pageProps = config.pageProps;
+
+    return (
+      <ContentPageSkeleton
+        pageProps={pageProps}
+        content={this.renderContent()}
+      />
+    )
+  }
+
+  renderContent() {
+    return (
+      <div>
+        {/* <FuturePageSectionCheetahAmbassadors config={config} /> */}
+      </div>
+    )
+  }
+}
---
+++ src/components/biology/config.js
@@ -0,0 +1,30 @@
+/**
+ * biology/config.js
+ * Chewbaaka
+ *
+ * Author   : Tomiko
+ * Created  : Jul 15, 2020
+ * Updated  : Jul 15, 2020
+ */
+
+export const config = {
+  pageProps: {
+    coverImage: "",
+    title: "Biology",
+    subtitle: "Subtitle TBD ...",
+    pageMenuItems: [
+      "Lifecycle",
+      "Anatomy",
+    ],
+  },
+  contentPageSections: {
+    section_Lifecycle: {
+      intro: {
+        title: "Lifecycle",
+        content: "Content TBD ..."
+      },
+      subsections: {
+      },
+    },
+  }
+};
