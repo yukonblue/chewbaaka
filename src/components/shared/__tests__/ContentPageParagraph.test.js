3cf0e4768ec5d5061b4777881846c02383cf77b2
Tomiko <554685+tetrachrome@users.noreply.github.com>
2020-07-14 21:38:57 -0700

    Make this a constant

--- src/components/shared/__tests__/ContentPageParagraph.test.js
+++ src/components/shared/__tests__/ContentPageParagraph.test.js
@@ -17,6 +17,12 @@ import ContentPageParagraph from '../ContentPageParagraph'
 
 const content = "Hello, World. This is a ContentPageParagraph component";
 
+/**
+ * Use a constant for this and have a test case for it so if it changes
+ * when I won't forget.
+ */
+const __ContentPageParagraphStyleClassName__ = "ContentPageContentParagraphText";
+
 test('renders ContentPageParagraph component', () => {
   render(
     <ContentPageParagraph>
@@ -27,7 +33,7 @@ test('renders ContentPageParagraph component', () => {
   const component = screen.getByText(content);
   expect(component).toBeInTheDocument();
 
-  expect(component.classList.contains("ContentPageContentParagraphText")).toBe(true);
+  expect(component.classList.contains(__ContentPageParagraphStyleClassName__)).toBe(true);
 });
 
 test('ContentPageParagraph component snapshot', () => {
