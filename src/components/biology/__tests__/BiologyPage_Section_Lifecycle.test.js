a0914a76ed35c65606ba896a0e23b53fe0deffc7
Tomiko <554685+tetrachrome@users.noreply.github.com>
2020-07-15 01:00:18 -0700

    Add unit test and snapshot test for 'Lifecycle' section

---
+++ src/components/biology/__tests__/BiologyPage_Section_Lifecycle.test.js
@@ -0,0 +1,30 @@
+/**
+ * BiologyPage_Section_Lifecycle.test.js
+ * Chewbaaka
+ *
+ * Author   : Tomiko
+ * Created  : Jul 15, 2020
+ * Updated  : Jul 15, 2020
+ */
+
+import React from 'react'
+
+import { render } from '@testing-library/react'
+
+import { config } from '../config'
+
+import renderer from 'react-test-renderer'
+
+import BiologyPageSectionLifecyle from '../BiologyPage_Section_Lifecycle'
+
+test('renders BiologyPageSectionLifecyle component', () => {
+  render(<BiologyPageSectionLifecyle config={config}/>);
+});
+
+test('BiologyPageSectionLifecyle component snapshot', () => {
+  const tree = renderer
+    .create(
+      <BiologyPageSectionLifecyle config={config}/>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
