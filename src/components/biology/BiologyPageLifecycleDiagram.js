4a079612cff1350d4efaa66f259164b1f19d25dc
Tomiko <554685+tetrachrome@users.noreply.github.com>
2020-07-16 02:35:07 -0700

    Make this a bit easier to read

--- src/components/biology/BiologyPageLifecycleDiagram.js
+++ src/components/biology/BiologyPageLifecycleDiagram.js
@@ -20,8 +20,12 @@ import image_lifecycle_diagram from './assets/Lifecycle_Diagram_640x675.png'
 export default function BiologyPageLifecycleDiagram() {
   return (
     <div className={getElementStyleClassName("BiologyPageLifecycleDiagramOuterContainer")}>
-      <h3 className="ContentPageSubsectionTitle TextCentered">The Cycle of Life</h3>
-      <p className="BiologyPageLifecycleDiagramSubtitle TextCentered">There are four stages in the lifecycle of the cheetah.</p>
+      <h3 className="ContentPageSubsectionTitle TextCentered">
+        The Cycle of Life
+      </h3>
+      <p className="BiologyPageLifecycleDiagramSubtitle TextCentered">
+        There are four stages in the lifecycle of the cheetah.
+      </p>
       <p className="BiologyPageLifecycleDiagramContentText TextCentered">
         <b>Stage 1</b>: 90 to 95 days pregnancy; <b>Stage 2</b>: 6 weeks to 18 months;
       </p>
