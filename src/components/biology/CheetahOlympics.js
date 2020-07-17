0a2872ea2f1f9dd3f17331bb4e89074de7bf078b
Tomiko <554685+tetrachrome@users.noreply.github.com>
2020-07-17 01:15:20 -0700

    Design and implement `TextRevealWithTextCover`, `QuizBoard`, and `CheetahOlympics` components

---
+++ src/components/biology/CheetahOlympics.js
@@ -0,0 +1,137 @@
+/**
+ * CheetahOlympics.js
+ * Chewbaaka
+ *
+ * Author   : Tomiko
+ * Created  : Jul 17, 2020
+ * Updated  : Jul 17, 2020
+ */
+
+import React from 'react'
+
+import QuizBoard from '../shared/QuizBoard'
+
+import './CheetahOlympics.css'
+
+export default class CheetahOlympics extends React.Component {
+
+  static __ITEMS__ = [
+    {
+      title: "Lion",
+      description: "80 km/hr (2nd place)",
+      position: {
+        top: 40,
+        left: 40
+      }
+    },
+    {
+      title: "Human",
+      description: "45 km/hr (7th place)",
+      position: {
+        top: 40,
+        left: 280
+      }
+    },
+    {
+      title: "Zebra",
+      description: "58 km/hr (6th place) ",
+      position: {
+        top: 40,
+        left: 520
+      }
+    },
+    {
+      title: "Black Mamba",
+      description: "32 km/hr (9th place)",
+      position: {
+        top: 240,
+        left: 40
+      }
+    },
+    {
+      title: "Wildebeest",
+      description: "78 km/hr (3rd place)",
+      position: {
+        top: 240,
+        left: 280
+      }
+    },
+    {
+      title: "Cheetah",
+      description: "110 km/hr (1st place 🏆)",
+      position: {
+        top: 240,
+        left: 520
+      }
+    },
+    {
+      title: "Race Horse",
+      description: "60 km/h (5th place)",
+      position: {
+        top: 440,
+        left: 40
+      }
+    },
+    {
+      title: "Elephant",
+      description: "65 km/hr (4th place)",
+      position: {
+        top: 440,
+        left: 280
+      }
+    },
+    {
+      title: "Warthog",
+      description: "40 km/hr (8th place)",
+      position: {
+        top: 440,
+        left: 520
+      }
+    },
+    {
+      title: "Jackal",
+      description: "16 km/hr (10th place)",
+      position: {
+        top: 640,
+        left: 280
+      }
+    },
+    {
+      title: "Second fastest animal in the world",
+      description: "Pronghorn antelope, a North American species, sprints up to 86 km/hr.",
+      position: {
+        top: 840,
+        left: 40
+      }
+    },
+    {
+      title: "Fastest animal in the world",
+      description: "Peregrine falcon, a bird species, dives at 360km/hr.",
+      fontSize: 16,
+      position: {
+        top: 840,
+        left: 520
+      }
+    },
+  ]
+
+  render() {
+    return (
+      <div className="CheetahOlympicsOuterContainer">
+        <h4 className="CheetahOlympicsTitle">Cheetah Olympics</h4>
+
+        <p>Frankie Fredericks, Namibian Olympic silver medal sprinter, ran 200 meters in 19.92 seconds.</p>
+
+        <p>The cheetah can run the same distance in 6.54 seconds.</p>
+
+        <p className="CheetahOlympicsSubtitle">Who will win the race?</p>
+        <p>Which is the fastest land animal in Africa?</p>
+        <QuizBoard
+          width={720}
+          height={1200}
+          items={CheetahOlympics.__ITEMS__}
+        />
+      </div>
+    );
+  }
+}
---
+++ src/components/shared/QuizBoard.js
@@ -0,0 +1,48 @@
+/**
+ * QuizBoard.js
+ * Chewbaaka
+ *
+ * Author   : Tomiko
+ * Created  : Jul 17, 2020
+ * Updated  : Jul 17, 2020
+ */
+
+import React from 'react'
+
+import TextRevealWithTextCover from './TextRevealWithTextCover'
+
+import { getElementStyleClassName } from '../../styling/styling'
+
+import './QuizBoard.css'
+
+export default class QuizBoard extends React.Component {
+
+  render() {
+    const componentDimensionStyle = {
+      width: this.props.width,
+      height: this.props.height
+    };
+  
+    const items = this.props.items.map(this.renderItem);
+
+    return (
+      <div style={componentDimensionStyle} className={getElementStyleClassName("QuizBoardOuterContainer")}>
+        <div className="QuizBoardInnerContainer">
+          {items}
+        </div>
+      </div>
+    );
+  }
+
+  renderItem(item, idx) {
+    return (
+      <div key={idx} className="QuizBoardItemContainer" style={item.position}>
+        <TextRevealWithTextCover
+          title={item.title}
+          description={item.description}
+          color="#6e9ce9"
+        />
+      </div>
+    );
+  }
+}
