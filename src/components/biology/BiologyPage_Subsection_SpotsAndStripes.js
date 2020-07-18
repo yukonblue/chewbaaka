a68b547b4e65b70e7e4c600b38b59473c73f0ad4
Tomiko <554685+tetrachrome@users.noreply.github.com>
2020-07-18 12:31:27 -0700

    Refine content in 'Spots and Stripes' subsection

--- src/components/biology/BiologyPage_Subsection_SpotsAndStripes.js
+++ src/components/biology/BiologyPage_Subsection_SpotsAndStripes.js
@@ -7,7 +7,7 @@
  * Updated  : Jul 18, 2020
  */
 
-import React from 'react';
+import React from 'react'
 
 import '../shared/ContentPageSharedStyles.css'
 
@@ -17,7 +17,12 @@ import {
   ContentPageSubsectionParagraphsContentBinder
 } from '../shared/ContentPageSubsectionContentBinder'
 
+import ImageView from '../shared/ImageView'
+
 import BigCatSpotsIllustration from './BigCatSpotsIllustration'
+
+import image_cheetah_mantle from './assets/Cheetah_Mantle.jpg'
+import image_cheetah_and_honey_badger from './assets/cheetah_and_honey_badger.jpg'
 
 export default class BiologyPageSubsectionSpotsAndStripes extends React.Component {
 
@@ -45,6 +50,23 @@ export default class BiologyPageSubsectionSpotsAndStripes extends React.Componen
     return (
       <div className="">
         {ContentPageSubsectionParagraphsContentBinder(this.state.subsectionConfig.contents)}
+
+        <div className="Centered VerticalCushionPadding" style={{width: 1080}}>
+          <div className="FloatLeft">
+            <ImageView
+              image={image_cheetah_mantle}
+              caption="Mantle on cheetah cub's back provides a form of camouflage that's critical to its survival."
+              width={360}
+              height={360}
+            />
+          </div>
+          <ImageView
+            image={image_cheetah_and_honey_badger}
+            caption="The cheetah's mantle also provides a form of “mimicry” that it can use to deter predators."
+            width={720}
+            height={360}
+          />
+        </div>
 
         <BigCatSpotsIllustration />
       </div>
