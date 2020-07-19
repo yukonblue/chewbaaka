13d9b43cb17c1168361d479bf1c8afa51d2a31fc
Tomiko <554685+tetrachrome@users.noreply.github.com>
2020-07-19 14:49:47 -0700

    Add unit test for 'Physiology' section

---
+++ src/components/biology/__tests__/BiologyPage_Section_Physiology.test.js
@@ -0,0 +1,37 @@
+/**
+ * BiologyPage_Section_Physiology.test.js
+ * Chewbaaka
+ *
+ * Author   : Tomiko
+ * Created  : Jul 19, 2020
+ * Updated  : Jul 19, 2020
+ */
+
+import React from 'react'
+
+import { render } from '@testing-library/react'
+
+import { config } from '../config'
+
+import renderer from 'react-test-renderer'
+
+import { RUN_TEST_NEVER } from '../../../testing/testing'
+
+import BiologyPageSectionPhysiology from '../BiologyPage_Section_Physiology'
+
+test('renders BiologyPageSectionPhysiology component', () => {
+  render(<BiologyPageSectionPhysiology config={config}/>);
+});
+
+/**
+ * Skip this test for now.
+ */
+RUN_TEST_NEVER(() => {
+  test('BiologyPageSectionPhysiology component snapshot', () => {
+    const tree = renderer
+      .create(
+        <BiologyPageSectionPhysiology config={config}/>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
