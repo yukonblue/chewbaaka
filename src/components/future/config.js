5b3b51a6546c4bbd38a30d550418a8879a039d12
Tomiko <554685+tetrachrome@users.noreply.github.com>
2020-07-22 21:03:18 -0700

    Correct typo in page content

--- src/components/future/config.js
+++ src/components/future/config.js
@@ -45,7 +45,7 @@ export const config = {
               "cheetah. I have a special place where I can run like the wind.",
             paragraph_Mekondyo:
               'Hello, my name is Mekondyo. Mekondyo means "struggle" in the ' +
-              "Oshiwambo language. I was born on a farm northof Otjiwarango " +
+              "Oshiwambo language. I was born on a farm north of Otjiwarango " +
               "but I now live on farmland west of Otjiwarango. It is very " +
               "beautiful. I can see the Waterberg Plateau far away. " +
               "I am 5 years old now and I will tell you my story as you " +
