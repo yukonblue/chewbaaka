28851d32a218a2d7223650579c2af83c6733b1fa
Tomiko <554685+tetrachrome@users.noreply.github.com>
2020-07-22 23:33:10 -0700

    Fix position and styling issues of `CircularImageButton` component and content page ToC menu items

--- src/components/shared/CircularImageButton.js
+++ src/components/shared/CircularImageButton.js
@@ -4,26 +4,30 @@
  *
  * Author   : Tomiko
  * Created  : Jul 06, 2020
- * Updated  : Jul 06, 2020
+ * Updated  : Jul 22, 2020
  */
 
-import React from 'react';
-
-import "semantic-ui-css/semantic.min.css";
+import React from 'react'
 
 import './CircularImageButton.css'
+
+import { getElementStyleClassName } from '../../styling/styling'
 
 export default class CircularImageButton extends React.Component {
 
   render() {
     return (
-      <div className="CircularImageButtonOuterContainer">
+      <div className={getElementStyleClassName("CircularImageButtonOuterContainer")}>
         <a href={this.props.href}>
           <div className="CircularImageButtonCore">
-            <img className="CircularImageButtonImg" src={this.props.image} alt={this.props.title} />
+            <img
+              className={getElementStyleClassName("CircularImageButtonImg")}
+              src={this.props.image}
+              alt={this.props.title}
+            />
           </div>
           <div>
-            <p className="CircularImageButtonTitle">
+            <p className={getElementStyleClassName("CircularImageButtonTitle")}>
               {this.props.title}
             </p>
           </div>
