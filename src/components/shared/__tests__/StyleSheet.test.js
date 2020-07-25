b7fcf725e8e6db04fd5cca905693693fa04c8bd8
Tomiko <554685+tetrachrome@users.noreply.github.com>
2020-07-25 11:40:37 -0700

    Remove log call in StyleSheet.test.js

--- src/components/shared/__tests__/StyleSheet.test.js
+++ src/components/shared/__tests__/StyleSheet.test.js
@@ -4,26 +4,30 @@
  *
  * Author   : Tomiko
  * Created  : Jul 04, 2020
- * Updated  : Jul 04, 2020
+ * Updated  : Jul 25, 2020
  */
 
-import React from 'react';
+import React from 'react'
 
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react'
 
-import StyleSheet from '../StyleSheet';
+import StyleSheet from '../StyleSheet'
 
 test('renders StyleSheet', () => {
   const styleSheetPath = "some-fake-stylesheet-path.css";
 
-  render(<StyleSheet styleSheetPath={styleSheetPath} />);
+  render(
+    <StyleSheet
+      styleSheetPath={styleSheetPath}
+    />
+  );
 
   var allLinkComponents = document.getElementsByTagName("link");
   expect(allLinkComponents.length).toBe(1);
 
   var linkComponent = allLinkComponents[0];
 
-  console.log(linkComponent.href);
+  // console.log(linkComponent.href);
 
   expect(linkComponent.href).toContain(styleSheetPath);
 });
