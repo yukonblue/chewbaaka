3c3db65c8e0d334fc2669e9c067487160da1ae85
Tomiko <554685+tetrachrome@users.noreply.github.com>
2020-07-26 09:38:25 -0700

    [Refactor][Cleanup] Use the same naming convention for content page cover image for all page cover image files

--- src/components/biology/config.js
+++ src/components/biology/config.js
@@ -4,12 +4,12 @@
  *
  * Author   : Tomiko
  * Created  : Jul 15, 2020
- * Updated  : Jul 25, 2020
+ * Updated  : Jul 26, 2020
  */
 
 export const config = {
   pageProps: {
-    coverImage: "biology_page_cover_image.jpg",
+    coverImage: "Biology_Page_Cover_Image.jpg",
     title: "Biology",
     subtitle: "Learn everything about what makes the cheetah so remarkable and unqiue.",
     pageMenuItems: [
