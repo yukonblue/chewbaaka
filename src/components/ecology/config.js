4fe73f2662a6a9879dc25cd1d130724c457d7d1f
Tomiko <554685+tetrachrome@users.noreply.github.com>
2020-07-26 22:20:18 -0700

    [Cleanup] Use consistent naming convention for cover image in Ecology page

--- src/components/ecology/config.js
+++ src/components/ecology/config.js
@@ -9,7 +9,7 @@
 
 export const config = {
   pageProps: {
-    coverImage: "savanna.jpg",
+    coverImage: "Ecology_Page_Cover_Image.jpg",
     title: "Ecology",
     subtitle: "Learn about the habitats and ecosystems that cheetahs live in, and their relationships with other species in the ecosystem.",
     pageMenuItems: [
