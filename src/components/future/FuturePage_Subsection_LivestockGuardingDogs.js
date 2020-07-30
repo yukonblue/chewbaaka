fbd0c98747d082586a0a0addc90e9ced64beaa1e
Tomiko <554685+tetrachrome@users.noreply.github.com>
2020-07-30 16:04:40 -0700

    [Style][Fix] Fix overflow dimension issue of livestock guarding dog image in 'Livestock Guarding Dogs' subsection

--- src/components/future/FuturePage_Subsection_LivestockGuardingDogs.js
+++ src/components/future/FuturePage_Subsection_LivestockGuardingDogs.js
@@ -4,7 +4,7 @@
  *
  * Author   : Tomiko
  * Created  : Jul 22, 2020
- * Updated  : Jul 29, 2020
+ * Updated  : Jul 30, 2020
  */
 
 import React from 'react'
@@ -21,9 +21,13 @@ import {
 
 import ContentPageSubsectionSubtitle from '../shared/ContentPageSubsectionSubtitle'
 
+import CenteredFullWidthContainer from '../shared/CenteredFullWidthContainer'
+
 import { kStringConstantCheetahConservationFund } from '../shared/constants'
 
 import ImageView from '../shared/ImageView'
+
+import FluidImageWrapper from '../shared/FluidImageWrapper'
 
 import image_CCF_LGD from './assets/CCF_LGD.jpg'
 import image_CCF_anatolian_shepherd_puppies from './assets/CCF_anatolian_shepherd_puppies.jpg'
@@ -70,13 +74,12 @@ export default class FuturePageSubsectionLivestockGuardingDogs extends React.Com
         </ContentPageSubsectionSubtitle>
         {ContentPageSubsectionParagraphsContentBinder(part.content)}
 
-        <div className="Centered" style={{width: 1200}}>
-          <img
+        <CenteredFullWidthContainer maxWidth={1200}>
+          <FluidImageWrapper
             src={image_CCF_LGD}
-            alt=""
-            style={{width: 1200}}
+            alt="Livestock Guarding Dog"
           />
-        </div>
+        </CenteredFullWidthContainer>
       </ContentPageSubsectionPart>
     );
   }
