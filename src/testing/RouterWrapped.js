c2da49513b347f712c80032145c9040cdd59a68c
Tomiko <554685+tetrachrome@users.noreply.github.com>
2020-08-06 21:50:34 -0700

    Add comment

--- src/testing/RouterWrapped.js
+++ src/testing/RouterWrapped.js
@@ -11,6 +11,14 @@ import React from 'react'
 
 import { BrowserRouter } from 'react-router-dom'
 
+/**
+ * Wraps a component under `BrowserRouter`.
+ *
+ * This is mostly needed in unit tests where <Link>
+ * and other react-router components need to be
+ * wrapped under a instance of concrete class
+ * derived from <Router>.
+ */
 export default function RouterWrapped(children) {
   return (
     <BrowserRouter>
