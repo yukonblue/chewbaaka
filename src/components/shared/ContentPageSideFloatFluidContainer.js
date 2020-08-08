d802db18712435abcc4da3b1e6a9817477846478
Tomiko <554685+tetrachrome@users.noreply.github.com>
2020-08-08 13:03:42 -0700

    Fix for Issue #94 (#127)
    This patch fixes the issue by removing the 50% width specification in `ContentPageSideFloatFluidContainer` column part styles.

--- src/components/shared/ContentPageSideFloatFluidContainer.js
+++ src/components/shared/ContentPageSideFloatFluidContainer.js
@@ -4,43 +4,49 @@
  *
  * Author   : Tomiko
  * Created  : Jul 26, 2020
- * Updated  : Jul 31, 2020
+ * Updated  : Aug 08, 2020
  */
 
 import React from 'react'
 
 import './ContentPageSharedStyles.css'
 
+import {
+  getElementStyleClassNames
+} from '../../styling/styling'
+
+if ( process.env.NODE_ENV === 'development' )
+  require('./ContentPageSideFloatFluidContainer-debug.css')
+
 export default class ContentPageSideFloatFluidContainer extends React.Component {
 
   render() {
-    const halfWidthClassName = "HalfWidth";
-
     const floatClassName = this.props.RTL ? "FloatLeft" : "FloatRight";
 
     const paddingClassName = this.props.RTL ? "HorizontalCusionPaddingRight" : "HorizontalCusionPaddingLeft";
 
-    let classNames = [floatClassName, paddingClassName, "BottomMargin20px"];
-
-    if (this.props.floatFixedSide) {
-      classNames.push(halfWidthClassName);
-    }
+    let classNames = ["ContentPageSideFloatFluidContainerFloatPartContainer",
+                      "BottomMargin20px",
+                      floatClassName,
+                      paddingClassName
+                    ];
 
     const oppositeFloatClassName = this.props.floatFixedSide ? (this.props.RTL ? "FloatRight" : "FloatLeft") : "";
 
-    let oppositeClassNames = [];
+    let oppositeClassNames = ["ContentPageSideFloatFluidContainerFixedPartContainer"];
 
     if (this.props.floatFixedSide) {
       oppositeClassNames.push(oppositeFloatClassName);
-      oppositeClassNames.push(halfWidthClassName);
     }
 
     return (
-      <div className="OverflowHidden">
-        <div className={classNames.join(" ")}>
+      <div className={getElementStyleClassNames(["ContentPageSideFloatFluidContainerOuterContainer",
+                                                 "OverflowHidden"])}
+      >
+        <div className={getElementStyleClassNames(classNames)}>
           {this.props.floatPart}
         </div>
-        <div className={oppositeClassNames.join(" ")}>
+        <div className={getElementStyleClassNames(oppositeClassNames)}>
           {this.props.fixedPart}
         </div>
       </div>
