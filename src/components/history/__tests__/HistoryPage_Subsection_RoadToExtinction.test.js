96ef79b313ffd778b7f3c69122fbde6d67436e3a
Tomiko <554685+tetrachrome@users.noreply.github.com>
2020-08-17 21:49:31 -0700

    Add unit test for 'Road To Extinction' subsection

---
+++ src/components/history/__tests__/HistoryPage_Subsection_RoadToExtinction.test.js
@@ -0,0 +1,45 @@
+/**
+ * HistoryPage_Subsection_RoadToExtinction.test.js
+ * Chewbaaka
+ *
+ * Author   : Tomiko
+ * Created  : Aug 17, 2020
+ * Updated  : Aug 17, 2020
+ */
+
+import React from 'react'
+
+import { render } from '@testing-library/react'
+
+import renderer from 'react-test-renderer'
+
+import { config } from '../config'
+
+import HistoryPageSubsectionRoadToExtinction from '../HistoryPage_Subsection_RoadToExtinction'
+
+import { RUN_TEST_NEVER } from '../../../testing/testing'
+
+const sectionConfig = config.contentPageSections["section_PopulationAndRange"];
+
+test('renders HistoryPageSubsectionRoadToExtinction component', () => {
+  render(
+    <HistoryPageSubsectionRoadToExtinction
+      sectionConfig={sectionConfig}
+    />
+  );
+});
+
+/**
+ * Skip this test for now.
+ */
+RUN_TEST_NEVER(() => {
+  test('HistoryPageSubsectionRoadToExtinction component snapshot', () => {
+    const tree = renderer
+      .create(
+        <HistoryPageSubsectionRoadToExtinction
+          sectionConfig={sectionConfig}
+        />
+      ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
