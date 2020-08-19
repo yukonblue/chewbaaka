dad53a30565dbe6dc242a887cd56065542e6271f
Tomiko <554685+tetrachrome@users.noreply.github.com>
2020-08-18 21:01:57 -0700

    Resurrect unit test for `ContentPageSkeleton`

--- src/components/shared/__tests__/ContentPageSkeleton.test.js
+++ src/components/shared/__tests__/ContentPageSkeleton.test.js
@@ -4,48 +4,78 @@
  *
  * Author   : Tomiko
  * Created  : Jul 06, 2020
- * Updated  : Aug 13, 2020
+ * Updated  : Aug 18, 2020
  */
 
 import React from 'react'
 
 import { render, screen } from '@testing-library/react'
 
+import renderer from 'react-test-renderer'
+
 import ContentPageSkeleton from '../ContentPageSkeleton'
 
 import RouterWrapped from '../../../testing/RouterWrapped'
 
-test('renders ContentPageSkeleton component', () => {
-  const pageProps = {
-    coverImage: "",
-    title: "Title",
-    subtitle: "This is a subtitle",
-    pageMenuItems: [
-      {
-        title: "Menu Item 1"
-      },
-      {
-        title: "Menu Item 2"
-      }
-    ],
-    pageTailNavMenu: {
-      prevPage: {
-        label: "Biology",
-        href: "/biology"
-      },
-      nextPage: {
-        label: "Future",
-        href: "/future"
-      }
+const pageProps = {
+  coverImage: "",
+  title: "Title",
+  subtitle: "This is a subtitle",
+  pageMenuItems: [
+    {
+      title: "Menu Item 1"
+    },
+    {
+      title: "Menu Item 2"
     }
-  };
+  ],
+  pageTailNavMenu: {
+    prevPage: {
+      label: "Biology",
+      href: "/biology"
+    },
+    nextPage: {
+      label: "Future",
+      href: "/future"
+    }
+  }
+};
 
+const contentPageIntro = {
+  title: "This is a title",
+  content: "This is some content",
+  image: {
+    filename: "cheetah-conservation-fund-logo-mini-min.jpg"
+  }
+};
+
+const imagesContext = () => {
+  return (imageName) => (null);
+};
+
+const renderInstance = () => (
+  RouterWrapped(
+    <ContentPageSkeleton
+      pageProps={pageProps}
+      contentPageIntro={contentPageIntro}
+      imagesContext={imagesContext}
+    />
+  )
+);
+
+test('renders ContentPageSkeleton component', () => {
   render(
-    RouterWrapped(
-      <ContentPageSkeleton pageProps={pageProps}/>
-    )
+    renderInstance()
   );
 
   const component = screen.getByTestId("ContentPageSkeletonComponentTestId");
   expect(component).toBeInTheDocument();
+});
+
+test('ContentPageSkeleton snapshot', () => {
+  const tree = renderer
+    .create(
+      renderInstance()
+    ).toJSON();
+  expect(tree).toMatchSnapshot();
 });
