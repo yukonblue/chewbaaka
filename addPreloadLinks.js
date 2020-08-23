227c11ed0aa4646a3ae6eca107674bc1affd5311
Tomiko <554685+tetrachrome@users.noreply.github.com>
2020-08-22 21:31:59 -0700

    Convert stylesheet links in index.html's head markup to 'preload' links
    This patch takes a script from CRA's discussion on this topic (at the URL below)
    and converts all stylesheet links in index.html's head markup to 'preload' links.
    This is run as part of the "build" step action specified in 'package.json'.
    
    Article on preloading resources:
    
    https://web.dev/preload-critical-assets/
    
    CRA issue and discussion:
    
    https://github.com/facebook/create-react-app/issues/3319

---
+++ addPreloadLinks.js
@@ -0,0 +1,49 @@
+/**
+ * addPreloadLink.js
+ *
+ * Converts a stylesheet <link> tags in a html page's head markup
+ * to be "preload" links.
+ *
+ * https://web.dev/preload-critical-assets/
+ *
+ * This script is taken from dicussion on this topic (CRA's issue)
+ *
+ * facebook / create-react-app
+ * Add preload to script and link tags in production builds #3319
+ * 
+ * https://github.com/facebook/create-react-app/issues/3319
+ */
+
+const fs = require('fs');
+const pathToEntry = './build/index.html';
+const bundlesRegExp = /\/static\/\w+\/\w+.[a-z0-9]+.[a-z0-9]+.\w{2,3}/g;
+const splitBy = '</title>';
+
+const builtHTMLContent = fs.readFileSync(pathToEntry).toString();
+const links = builtHTMLContent.match(bundlesRegExp);
+const parts = builtHTMLContent.split(splitBy);
+
+let fileWithPreload = [
+  parts[0],
+  splitBy,
+];
+
+links.forEach(link => {
+  let fileType = 'script';
+
+  if (/\.css$/.test(link)) {
+    fileType = 'style';
+  }
+
+  fileWithPreload = [
+    ...fileWithPreload,
+    `<link rel="preload" href="${link}" as="${fileType}">`,
+  ];
+});
+
+fileWithPreload = [
+  ...fileWithPreload,
+  parts[1],
+];
+
+fs.writeFileSync(pathToEntry, fileWithPreload.join(''));
