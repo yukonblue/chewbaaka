686c9ec0dfa9a639bf51119a3e5dd167c619439b
Tomiko <554685+tetrachrome@users.noreply.github.com>
2020-08-24 23:27:52 -0700

    Port to SSG architecture (#193)
    This patch is the first attempt to completely adopt a static-site generation architecture, by adopting the `react-snap` framework.

--- src/index.js
+++ src/index.js
@@ -4,22 +4,22 @@
  *
  * Author   : Tomiko
  * Created  : Jun 30, 2020
- * Updated  : Jul 01, 2020
+ * Updated  : Aug 21, 2020
  */
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { hydrate, render } from "react-dom";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'semantic-ui-css/semantic.min.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
