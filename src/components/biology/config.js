befd7647f310bc08dab0b82221d88eeb91c7cd9e
Tomiko <554685+tetrachrome@users.noreply.github.com>
2021-08-18 07:08:46 -0700

    Optimize content page assets - 08.18.21 - Part 1
    This patch is the first of the series of patches that attempt
    to optimize the assets and payloads across content pages.
    
    This patch optimizes the intro section background images of
    all content pages by introducing a Small variant intended
    for small mobile display sizes.

--- src/components/biology/config.js
+++ src/components/biology/config.js
@@ -4,7 +4,7 @@
  *
  * Author   : Tomiko
  * Created  : Jul 15, 2020
- * Updated  : Aug 29, 2020
+ * Updated  : Aug 18, 2021
  */
 
 export const config = {
@@ -60,7 +60,7 @@ export const config = {
             "The species name, “IIIjubatusIII”, means “maned”, referring to the mantle on a young cheetah’s back. " +
             "The English word, “cheetah”, comes from the Hindu word “chita” meaning the “spotted one”.",
     image: {
-      filename: "Biology_Page_Intro_Section_Background-min.png"
+      filenamePrefix: "Biology_Page_Intro_Section_Background"
     }
   },
   contentPageSections: {
--- src/components/shared/ContentPageIntroSectionGeneric.js
+++ src/components/shared/ContentPageIntroSectionGeneric.js
@@ -4,10 +4,12 @@
  *
  * Author   : Tomiko
  * Created  : Jul 24, 2020
- * Updated  : Aug 12, 2020
+ * Updated  : Aug 18, 2021
  */
 
 import React from 'react'
+
+import Media from 'react-media'
 
 import { getElementStyleClassName } from '../../styling/styling'
 
@@ -22,21 +24,47 @@ import getFormattedText from './TextFormatter.js'
 if ( process.env.NODE_ENV === 'development' )
   require('./ContentPageIntroSectionGeneric-debug.css')
 
+const __TEST__ = (process.env.NODE_ENV === 'test');
+
 export default class ContentPageIntroSectionGeneric extends React.Component {
 
   render() {
+    if ( __TEST__ ) {
+      return this.renderCore(null);
+    }
+
+    return (
+      <Media queries={{
+        small: "(max-width: 480px)",
+        large: "(min-width: 481px)"
+      }}>
+        {
+          matches => (this.renderCore(matches))
+        }
+      </Media>
+    );
+  }
+
+  renderCore(matches) {
     return (
       <div className={getElementStyleClassName("ContentPageIntroSectionGenericOuterContainer")}>
         <ContentPageIntroSectionTemplate
           title={this.props.contentPageIntro.title}
-          content={this.renderContent()}
+          content={this.renderContent(matches)}
         />
       </div>
     );
   }
 
-  renderContent() {
+  renderContent(matches) {
     const images = this.props.imagesContext();
+
+    const coverImageSizeSuffix = matches ? (matches.small ? "_S" : "_L") : "_L";
+
+    const ext = ".png";
+
+    const coverImageName = "./" + this.props.contentPageIntro.image.filenamePrefix + coverImageSizeSuffix + "-min" + ext;
+
     return (
       <div className={getElementStyleClassName("ContentPageIntroSectionGenericInnerContainer")}>
         <div className="ContentPageIntroSectionGenericIntroTextContainer">
@@ -47,7 +75,7 @@ export default class ContentPageIntroSectionGeneric extends React.Component {
         <div className="ContentPageIntroSectionGenericImgContainer">
           <img
             className={getElementStyleClassName("ContentPageIntroSectionGenericImg")}
-            src={images("./" + this.props.contentPageIntro.image.filename)}
+            src={images(coverImageName)}
             alt={this.props.contentPageIntro.title}
           />
         </div>
