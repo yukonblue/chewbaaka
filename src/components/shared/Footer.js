7f275f4f656f6c80f880fc26829f2d18856c28b4
Tomiko <554685+tetrachrome@users.noreply.github.com>
2022-06-14 23:44:24 -0400

    Drop the word 'from'

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -29,7 +29,7 @@ export default function Footer(props) {
   const renderOptionalAuthorName = () => (
     props.appConfig && props.appConfig.author ? (
       <p>
-        Crafted with <span role="img" aria-label="heart">&#10084;&#65039;</span> by {props.appConfig.author} from &#127464;&#127462;
+        Crafted with <span role="img" aria-label="heart">&#10084;&#65039;</span> by {props.appConfig.author} &#127464;&#127462;
       </p>
     ) : null
   );
