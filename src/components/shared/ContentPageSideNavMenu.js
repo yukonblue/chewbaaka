2d7d5d8167865d5ea5d5ed38793e234b54735b5d
Tomiko <554685+tetrachrome@users.noreply.github.com>
2022-06-14 23:07:01 -0400

    Introduce Design 2 styles to `ContentPageSideNavMenu` component (#293)
    This patch introduces a refined set of styles to the `ContentPageSideNavMenu` component, with the most significant change to introduce links to content pages, alongside with the existing links that point to sections within the current page.

--- src/components/shared/ContentPageSideNavMenu.js
+++ src/components/shared/ContentPageSideNavMenu.js
@@ -4,12 +4,12 @@
  *
  * Author   : Tomiko
  * Created  : Jul 06, 2020
- * Updated  : Aug 18, 2020
+ * Updated  : Jun 13, 2022
  */
 
-import React from 'react'
+import React, { Fragment } from 'react'
 
-import { Menu } from 'semantic-ui-react'
+import { Menu, List } from 'semantic-ui-react'
 
 import { ContentPageSectionTitleToAnchorId } from './ContentPageSectionAnchor'
 
@@ -19,12 +19,17 @@ import 'semantic-ui-css/semantic.min.css'
 
 import './ContentPageSideNavMenu.css'
 
+import { Design2CommonRoundedBorderRadiusStyle } from './Design2_CommonUtils'
+
+import { combineStyles } from './Utils'
+
 export default class ContentPageSideNavMenu extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
       // contextRef: props.contextRef,
+      title: props.title,
       items : props.items,
       activeItem : props.items[0]
     }
@@ -39,37 +44,59 @@ export default class ContentPageSideNavMenu extends React.Component {
   }
 
   render() {
-    var menuItems = [];
+    var sectionMenuItems = [];
 
     for (const [idx, item] of this.state.items.entries()) {
-      menuItems.push(
-        <Menu.Item
+      sectionMenuItems.push(
+        <List.Item
           key={idx}
           active={this.state.activeItem === item}
           href={"#"+ContentPageSectionTitleToAnchorId(item)}
           onClick={this.handleItemClick}
         >
-          <span className="ContentPageSideNavMenuItem">
+          <span className="ContentPageSideNavMenuSectionMenuItem">
             {item}
           </span>
-        </Menu.Item>
+        </List.Item>
       );
     }
 
+    const getTopMenuItem = (idx, item) => {
+      if (item === this.props.title) {
+        return (
+          <Fragment key={idx}>
+            <p className="ContentPageSideNavMenuTopItemInactive">
+              {item}
+            </p>
+            <List link className="ContentPageSideNavMenuSectionMenuList">
+              {sectionMenuItems}
+            </List>
+          </Fragment>
+        );
+      } else {
+        return (
+          <Menu.Item
+            key={idx}
+            active={true}
+            href={"/" + (item.toLowerCase() + "/")}
+          >
+            <span className="ContentPageSideNavMenuTopItem">
+              {item}
+            </span>
+          </Menu.Item>
+        );
+      }
+    };
+
+    const contentPageNames = ["History", "Biology", "Ecology", "Future"]
+
     return (
       <div
-        className={getElementStyleClassName("ContentPageSideNavMenuOuterContainer")}
+        className={combineStyles(getElementStyleClassName("ContentPageSideNavMenuOuterContainer"), Design2CommonRoundedBorderRadiusStyle)}
         data-testid="ContentPageSideNavMenuComponentTestId"
       >
         <Menu text vertical className="ContentPageSideNavMenuInnerMenu">
-          <Menu.Item header>
-            <span className="ContentPageSideNavMenuHeader">
-              {this.props.title}
-            </span>
-          </Menu.Item>
-          <nav>
-            {menuItems}
-          </nav>
+          {contentPageNames.map( (item, idx) => getTopMenuItem(idx, item) )}
         </Menu>
       </div>
     );
